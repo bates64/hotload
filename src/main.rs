02c2e40cfedca74b67cc0784ed483fbdcddb812a
Alex Bates <alex@nanaian.town>
2024-01-30 22:57:56 +0000

    parse elf file

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
-fn main() {
-    println!("Hello, world!");
+mod program;
+
+use clap::Parser;
+use std::path::PathBuf;
+
+/// Hot code loading (dynamic software updating) for Nintendo 64 development
+#[derive(Parser, Debug)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    /// ELF file to watch
+    elf: PathBuf,
+}
+
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let args = Args::parse();
+
+    // Parse ELF file
+    let elf_file = std::fs::read(args.elf)?;
+    let elf = goblin::elf::Elf::parse(&elf_file)?;
+    let program = program::Program::from(&elf);
+
+    println!("Loaded {} items", program.items.len());
+
+    Ok(())
 }
---
+++ src/program.rs
@@ -0,0 +1,59 @@
+use goblin::elf::{section_header, Elf};
+use paris::warn;
+use std::collections::HashMap;
+
+#[derive(Debug, Clone)]
+pub struct Program<'a> {
+    pub items: HashMap<&'a str, Item<'a>>,
+}
+
+#[derive(Debug, Clone)]
+pub struct Item<'a> {
+    section_name: Option<&'a str>,
+    ram_addr: u64,
+    rom_addr: u64,
+    size: u64,
+}
+
+impl<'a> From<&Elf<'a>> for Program<'a> {
+    fn from(elf: &Elf<'a>) -> Self {
+        let mut items = HashMap::new();
+
+        for sym in &elf.syms {
+            let name = elf.strtab.get_at(sym.st_name);
+
+            // Ignore symbols that are not in a section
+            match sym.st_shndx as u32 {
+                section_header::SHN_UNDEF
+                | section_header::SHN_LOPROC
+                | section_header::SHN_HIPROC
+                | section_header::SHN_ABS
+                | section_header::SHN_COMMON
+                | section_header::SHN_HIRESERVE => continue,
+                _ => {}
+            };
+
+            let section = &elf.section_headers[sym.st_shndx];
+            let section_name = elf.shdr_strtab.get_at(section.sh_name);
+
+            let sym_offset = sym.st_value - section.sh_addr;
+
+            // TODO: consider tracking st_type (sym::STT_* consts)
+
+            let item = Item {
+                section_name,
+                ram_addr: sym.st_value,
+                rom_addr: section.sh_offset + sym_offset,
+                size: sym.st_size,
+            };
+
+            if let Some(name) = name {
+                items.insert(name, item);
+            } else {
+                warn!("Symbol at index {} has no name, ignoring it", sym.st_name);
+            }
+        }
+
+        Self { items }
+    }
+}
