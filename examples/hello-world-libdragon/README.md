02055617af2a28b8f0e10e3e25d2f3e66982b92c
Alex Bates <alex@nanaian.town>
2024-01-31 00:36:49 +0000

    watch source files and rebuild automatically

---
+++ examples/hello-world-libdragon/README.md
@@ -0,0 +1,13 @@
+# hotload64 with libdragon
+
+This is an example of how to use hotload64 with libdragon.
+
+## Setup
+
+Download the toolchain with `npm install` and make sure you have hotload64 installed.
+
+## Watch
+
+```
+hotload64 --build "npm build" --elf build/hello.elf --src src
+```
--- src/main.rs
+++ src/main.rs
@@ -1,25 +1,59 @@
 mod program;
 
 use clap::Parser;
-use std::path::PathBuf;
+use notify_debouncer_mini::{new_debouncer, notify::RecursiveMode};
+use std::{path::PathBuf, time::Duration};
 
 /// Hot code loading (dynamic software updating) for Nintendo 64 development
 #[derive(Parser, Debug)]
 #[command(author, version, about, long_about = None)]
 struct Args {
-    /// ELF file to watch
+    /// Build system command to run (e.g. `make`, `ninja`, `libdragon build`)
+    #[clap(short, long)]
+    build: String,
+
+    /// ELF file that is output from build command
+    #[clap(short, long)]
     elf: PathBuf,
+
+    /// Source files and/or directories to recursively watch for changes
+    #[clap(short, long)]
+    src: Vec<PathBuf>,
 }
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let args = Args::parse();
 
     // Parse ELF file
-    let elf_file = std::fs::read(args.elf)?;
+    let elf_file = std::fs::read(&args.elf)?;
     let elf = goblin::elf::Elf::parse(&elf_file)?;
     let program = program::Program::from(&elf);
-
     println!("Loaded {} items", program.items.len());
+
+    // Watch for source changes
+    let (tx, rx) = std::sync::mpsc::channel();
+    let mut debouncer = new_debouncer(Duration::from_millis(10), tx)?;
+    let watcher = debouncer.watcher();
+    for path in args.src {
+        watcher.watch(&path, RecursiveMode::Recursive)?;
+    }
+
+    for result in rx {
+        result?; // Propagate errors
+
+        // Rebuild the project
+        std::process::Command::new("sh")
+            .arg("-c")
+            .arg(&args.build)
+            .status()
+            .expect("Failed to execute build system");
+
+        // Reload the program
+        let elf_file = std::fs::read(&args.elf)?;
+        let elf = goblin::elf::Elf::parse(&elf_file)?;
+        let new_program = program::Program::from(&elf);
+        println!("New program! Loaded {} items", new_program.items.len());
+    }
 
     Ok(())
 }
