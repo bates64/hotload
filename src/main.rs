4b3a407a484069ce0fd031e92afb81dfae3f55a0
Alex Bates <alex@bates64.com>
2024-02-17 17:50:54 +0000

    remove tui

--- src/main.rs
+++ src/main.rs
@@ -5,19 +5,9 @@ mod patch;
 mod program;
 
 use clap::Parser;
-use crossterm::{
-    event::{self, KeyCode, KeyEventKind},
-    terminal::{disable_raw_mode, enable_raw_mode, EnterAlternateScreen, LeaveAlternateScreen},
-    ExecutableCommand,
-};
 use notify_debouncer_mini::{new_debouncer, notify::RecursiveMode};
 use paris::error; // TODO: stop using paris, use a ratatui widget
-use ratatui::{
-    prelude::{CrosstermBackend, Stylize, Terminal},
-    widgets::Paragraph,
-};
-use std::io::stdout;
-use std::sync::mpsc::{channel, TryRecvError};
+use std::sync::mpsc::channel;
 use std::{path::PathBuf, time::Duration};
 
 /// Hot code loading (dynamic software updating) for Nintendo 64 development
@@ -80,83 +70,35 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     // Everything is ready.
 
-    // Initialise TUI
-    stdout().execute(EnterAlternateScreen)?;
-    enable_raw_mode()?;
-    let mut terminal = Terminal::new(CrosstermBackend::new(stdout()))?;
-    terminal.clear()?;
-
     let mut diff;
 
-    loop {
-        terminal.draw(|frame| {
-            let area = frame.size();
-            frame.render_widget(
-                Paragraph::new("Hello Ratatui! (press 'q' to quit)")
-                    .white()
-                    .on_blue(),
-                area,
-            );
-        })?;
+    // On filesystem change, rebuild the project
+    for result in rx {
+        result?;
 
-        // Check for exit
-        if event::poll(std::time::Duration::from_millis(16))? {
-            if let event::Event::Key(key) = event::read()? {
-                match (key.kind, key.code) {
-                    (KeyEventKind::Press, KeyCode::Char('q')) => break,
-                    (KeyEventKind::Press, KeyCode::Esc) => break,
-                    (KeyEventKind::Press, KeyCode::Char('c'))
-                        if key.modifiers.contains(event::KeyModifiers::CONTROL) =>
-                    {
-                        break
-                    }
-                    _ => {}
-                }
-            }
+        // Rebuild the project
+        if let Err(error) = run_build_command(&args.build) {
+            error!("{}", error);
+            continue;
         }
 
-        // Check for filesystem change; if so, rebuild the project
-        match rx.try_recv() {
-            Ok(Ok(_)) => {
-                // TODO: thread
+        // Reload the program
+        let elf_file = std::fs::read(&args.elf)?;
+        let new_program = program::Program::new(&elf_file)?;
+        println!("New program! Loaded {} items", new_program.items.len());
+        println!("{}", program.items["main"].disassemble().unwrap());
 
-                // Rebuild the project
-                if let Err(error) = run_build_command(&args.build) {
-                    error!("{}", error);
-                    continue;
-                }
-
-                // Reload the program
-                let elf_file = std::fs::read(&args.elf)?;
-                let new_program = program::Program::new(&elf_file)?;
-                println!("New program! Loaded {} items", new_program.items.len());
-                println!("{}", program.items["main"].disassemble().unwrap());
-
-                diff = diff::diff(&program, &new_program);
-                if let Err(error) = patch::apply(&mut gdb, &diff) {
-                    error!("{}", error);
-                    continue;
-                }
-
-                // TODO: solve lifetimes
-                //program = new_program;
-            }
-            Ok(Err(error)) => {
-                // TODO: do this with ratatui
-                error!("fs error: {}", error);
-            }
-            Err(TryRecvError::Empty) => {}
-            Err(TryRecvError::Disconnected) => {
-                error!("filesystem died");
-                break;
-            }
+        diff = diff::diff(&program, &new_program);
+        if let Err(error) = patch::apply(&mut gdb, &diff) {
+            error!("{}", error);
+            continue;
         }
+
+        // TODO: solve lifetimes
+        //program = new_program;
     }
 
     emulator::try_kill();
-    stdout().execute(LeaveAlternateScreen)?;
-    disable_raw_mode()?;
-
     Ok(())
 }
 
@@ -171,14 +113,11 @@ fn run_build_command(command: &str) -> Result<(), Box<dyn std::error::Error>> {
     Ok(())
 }
 
-/// Set up a panic handler that restores the terminal state and kills the emulator before exiting.
-// https://ratatui.rs/how-to/develop-apps/panic-hooks/
+/// Set up a panic handler that kills the emulator before exiting.
 fn setup_panic_handler() {
     let original_hook = std::panic::take_hook();
     std::panic::set_hook(Box::new(move |panic_info| {
         emulator::try_kill();
-        crossterm::execute!(std::io::stderr(), crossterm::terminal::LeaveAlternateScreen).unwrap();
-        crossterm::terminal::disable_raw_mode().unwrap();
         original_hook(panic_info);
     }));
 }
